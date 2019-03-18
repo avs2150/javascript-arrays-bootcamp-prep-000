c9c14695242b88d00c7877657c4df5cb6ec7bb9d
ANASTASIA V SIDORENKOVA <avs2150@columbia.edu>
2019-03-18 05:53:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,1 @@
 Var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-
-Function addElementToBeginningOfArray(array, element){
-return [element, ...array]
-}
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element);
-  return array;
-}
